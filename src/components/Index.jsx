134d4996a6b613a3784de74f3f83a1b20c688a5d
MacBookPro <matthewpalmer9@outlook.com>
2020-10-31 08:20:00 -0500

    Added a header image and brand message to the front page with minimal styling.

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
-import '../styles/temp.css';
+import '../main.css';
+import '../styles/index.css';
+
 
 export default class Index extends Component {
     render() {
         return (
-            <div className="greet">
-                Hello from Landing Page
+            <div className="lp-container">
+                <div className="lp-container-header">
+                    <h1>Welcome to The Toolbox</h1>
+                    <div id="header-split"></div>
+                    <h3>We get it! Life can be difficult sometimes. Take control today with our free tools built for hard-working people like you.</h3>
+                </div>
+                <div className="undraw-laptop">
+                    <img src="./laptop.png"></img>
+                </div>
             </div>
         )
     }
